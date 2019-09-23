df918a15360c953ad758e34226ce1996a8214e25
Anderson688 <51816230+Anderson688@users.noreply.github.com>
2019-09-23 22:58:05 +0530

    Update Radius.cpp

--- Lab_post-mid_1/Radius.cpp
+++ Lab_post-mid_1/Radius.cpp
@@ -1,104 +1,78 @@
-// C++ implementation of worst case linear time algorithm 
-// to find k'th smallest element 
-#include<iostream> 
-#include<algorithm> 
-#include<climits> 
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <math.h>
 
-using namespace std; 
+using namespace std;
 
-/*int partition(int arr[], int l, int r, int k); 
-*/
-// A simple function to find median of arr[]. This is called 
-// only for an array of size 5 in this program. 
-int findMedian(int arr[], int n) 
-{ 
-	sort(arr, arr+n); // Sort the array 
-	return arr[n/2]; // Return middle element 
-} 
+void swap(long long *a, long long *b)
+{
+	long long temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-// Returns k'th smallest element in arr[l..r] in worst case 
-// linear time. ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT 
-int kthSmallest(int arr[], int l, int r, int k) 
-{ 
-	// If k is smaller than number of elements in array 
-	if (k > 0 && k <= r - l + 1) 
-	{ 
-		int n = r-l+1; // Number of elements in arr[l..r] 
+long long partition(long long arr[], long long l, long long r, long long x)
+{
+	long long i;
+	for (i = l; i < r; i++)
+		if (arr[i] == x)
+            break;
+	swap(&arr[i], &arr[r]);
+	i = l;
+	for(long long j = l; j <= r-1; j++)
+	{
+		if(arr[j] <= x)
+		{
+			swap(&arr[i], &arr[j]);
+			i++;
+		}
+	}
+	swap(&arr[i], &arr[r]);
+	return i;
+}
 
-		// Divide arr[] in groups of size 5, calculate median 
-		// of every group and store it in median[] array. 
-		int i, median[(n+4)/5]; // There will be floor((n+4)/5) groups; 
-		for (i=0; i<n/5; i++) 
-			median[i] = findMedian(arr+l+i*5, 5); 
-		if (i*5 < n) //For last group with less than 5 elements 
-		{ 
-			median[i] = findMedian(arr+l+i*5, n%5); 
-			i++; 
-		}	 
+long long find_median(long long arr[], long long n)
+{
+	sort(arr, arr+n);
+	return arr[n/2];
+}
 
-		// Find median of all medians using recursive call. 
-		// If median[] has only one element, then no need 
-		// of recursive call 
-		int medOfMed = (i == 1)? median[i-1]: 
-								kthSmallest(median, 0, i-1, i/2); 
+long long median(long long arr[], long long l, long long r, long long k)
+{
+		long long n = r-l+1;
+		long long i, med[(n+4)/5];
+		for (i=0; i<n/5; i++)
+			med[i] = find_median(arr + l + i*5, 5);
+		if (i*5 < n)
+		{
+			med[i] = find_median(arr + l + i*5, n%5);
+			i++;
+		}
+		long long med_of_med = (i == 1)? med[i-1]: median(med, 0, i-1, i/2);
+		long long position = partition(arr, l, r, med_of_med);
+		if (position-l == k-1)
+			return arr[position];
+		if (position-l > k-1)
+			return median(arr, l, position-1, k);
+		return median(arr, position+1, r, k-position+l-1);
+}
 
-		// Partition the array around a random element and 
-		// get position of pivot element in sorted array 
-		/*int pos = partition(arr, l, r, medOfMed); 
-
-		// If position is same as k 
-		if (pos-l == k-1) 
-			return arr[pos]; 
-		if (pos-l > k-1) // If position is more, recur for left 
-			return kthSmallest(arr, l, pos-1, k); 
-
-		// Else recur for right subarray 
-		return kthSmallest(arr, pos+1, r, k-pos+l-1); */
-return medOfMed;
-	} 
-
-	// If k is more than number of elements in array 
-	return INT_MAX; 
-} 
-
-void swap(int *a, int *b) 
-{ 
-	int temp = *a; 
-	*a = *b; 
-	*b = temp; 
-} 
-
-// It searches for x in arr[l..r], and partitions the array 
-// around x. 
-/*int partition(int arr[], int l, int r, int x) 
-{ 
-	// Search for x in arr[l..r] and move it to end 
-	int i; 
-	for (i=l; i<r; i++) 
-		if (arr[i] == x) 
-		break; 
-	swap(&arr[i], &arr[r]); 
-
-	// Standard partition algorithm 
-	i = l; 
-	for (int j = l; j <= r - 1; j++) 
-	{ 
-		if (arr[j] <= x) 
-		{ 
-			swap(&arr[i], &arr[j]); 
-			i++; 
-		} 
-	} 
-	swap(&arr[i], &arr[r]); 
-	return i; 
-} */
-
-// Driver program to test above methods 
-int main() 
-{ 
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-	int n = sizeof(arr)/sizeof(arr[0]), k = 5; 
-	cout << "K'th smallest element is "
-		<< kthSmallest(arr, 0, n-1, k); 
-	return 0; 
-} 
+int main()
+{
+	long long t;
+	cin >> t;
+	while(t--)
+    {
+        long long points;
+        cin >> points;
+        long long x, y, dist[points];
+        for(long long i = 0; i < points; i++)
+        {
+            cin >> x >> y;
+            dist[i] = x*x + y*y;
+        }
+        cout << sqrt(median(dist, 0, points-1, (points+1)/2));
+    }
+	return 0;
+}
