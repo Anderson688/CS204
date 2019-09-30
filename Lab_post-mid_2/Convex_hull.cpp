2ed1199847bde0eaec380dbd89475f9e53c2abde
Anderson688 <51816230+Anderson688@users.noreply.github.com>
2019-09-30 23:10:03 +0530

    Update Convex_hull.cpp

--- Lab_post-mid_2/Convex_hull.cpp
+++ Lab_post-mid_2/Convex_hull.cpp
@@ -1,93 +1,60 @@
-// C++ implementation of the approach 
-#include <bits/stdc++.h> 
-#define llu long long int 
-using namespace std; 
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-struct Point { 
+using namespace std;
 
-	llu x, y; 
+struct Point
+{
+	long long int x, y;
+	bool operator <(Point p)
+	{
+		return x < p.x || (x == p.x && y < p.y);
+	}
+};
 
-	bool operator<(Point p) 
-	{ 
-		return x < p.x || (x == p.x && y < p.y); 
-	} 
-}; 
+long long int cross_product(Point O, Point A, Point B)
+{
+	return (A.x-O.x)*(B.y-O.y) - (A.y-O.y)*(B.x-O.x);
+}
 
-// Cross product of two vectors OA and OB 
-// returns positive for counter clockwise 
-// turn and negative for clockwise turn 
-llu cross_product(Point O, Point A, Point B) 
-{ 
-	return (A.x - O.x) * (B.y - O.y) 
-		- (A.y - O.y) * (B.x - O.x); 
-} 
+vector<Point> convex_hull(vector<Point> A)
+{
+	int n = A.size(), k = 0;
+	if(n <= 3)
+		return A;
+	vector<Point> ans(2*n);
+    	sort(A.begin(), A.end());
+	for(int i = 0; i < n; ++i)
+   	{
+		while(k >= 2 && cross_product(ans[k-2], ans[k-1], A[i]) <= 0)
+			k--;
+		ans[k++] = A[i];
+	}
+	for(size_t i = n-1, t = k+1; i > 0; --i)
+	{
+        while(k >= t && cross_product(ans[k-2], ans[k-1], A[i-1]) <= 0)
+			k--;
+		ans[k++] = A[i-1];
+	}
+	ans.resize(k-1);
+	return ans;
+}
 
-// Returns a list of points on the convex hull 
-// in counter-clockwise order 
-vector<Point> convex_hull(vector<Point> A) 
-{ 
-	int n = A.size(), k = 0; 
-
-	if (n <= 3) 
-		return A; 
-
-	vector<Point> ans(2 * n); 
-
-	// Sort points lexicographically 
-	sort(A.begin(), A.end()); 
-
-	// Build lower hull 
-	for (int i = 0; i < n; ++i) { 
-
-		// If the point at K-1 position is not a part 
-		// of hull as vector from ans[k-2] to ans[k-1] 
-		// and ans[k-2] to A[i] has a clockwise turn 
-		while (k >= 2 && cross_product(ans[k - 2], 
-						ans[k - 1], A[i]) <= 0) 
-			k--; 
-		ans[k++] = A[i]; 
-	} 
-
-	// Build upper hull 
-	for (size_t i = n - 1, t = k + 1; i > 0; --i) { 
-
-		// If the point at K-1 position is not a part 
-		// of hull as vector from ans[k-2] to ans[k-1] 
-		// and ans[k-2] to A[i] has a clockwise turn 
-		while (k >= t && cross_product(ans[k - 2], 
-						ans[k - 1], A[i - 1]) <= 0) 
-			k--; 
-		ans[k++] = A[i - 1]; 
-	} 
-
-	// Resize the array to desired size 
-	ans.resize(k - 1); 
-
-	return ans; 
-} 
-
-// Driver code 
-int main() 
-{ 
+int main()
+{
 	int T;
 	cin >> T;
-	vector<Point> points; 
+	vector<Point> points;
 	int x, y;
-	// Add points 
 	for(int i = 0; i < T; i++)
 	{
 		cin >> x >> y;
 		points.push_back({x, y});
 	}
-
-	// Find the convex hull 
-	vector<Point> ans = convex_hull(points); 
-
-	// Print the convex hull 
-	cout << ans.size() << endl << ans[0].x << " " << ans[0].y << endl;
-	for (int i = ans.size()-1; i > 0; i--) 
-		cout << ans[i].x << " "
-			<< ans[i].y << endl; 
-
-	return 0; 
-} 
+	vector<Point> ans = convex_hull(points);
+    	cout << ans.size() << endl << ans[0].x << " " << ans[0].y << endl;
+	for (int i = ans.size()-1; i > 0; i--)
+		cout << ans[i].x << " " << ans[i].y << endl;
+	return 0;
+}
