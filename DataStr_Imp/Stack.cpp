4b09047261ef8bf1a616763538f8e4d223f7f0de
Anderson688 <51816230+Anderson688@users.noreply.github.com>
2019-11-02 00:44:17 +0530

    Update Stack.cpp

--- DataStr_Imp/Stack.cpp
+++ DataStr_Imp/Stack.cpp
@@ -7,27 +7,29 @@ using namespace std;
 class Stack
 {
     private:
-        int size;
+        int capacity;
         int top;
         int* array;
     public:
-        Stack(int size);
+        Stack(int capacity);
         void push(int x);
         void pop();
         void peek();
         void isEmpty();
+        void size();
+        void isFull();
 };
 
-Stack::Stack(int size)
+Stack::Stack(int capacity)
 {
     top = -1;
-    Stack::size = size;
-    array = new int[size];
+    Stack::capacity = capacity;
+    array = new int[capacity];
 }
 
 void Stack::push(int x)
 {
-    if(top < size-1)
+    if(top < capacity-1)
     {
         top = top+1;
         array[top] = x;
@@ -64,22 +66,40 @@ void Stack::isEmpty()
         cout << "Stack is empty" << endl;
 }
 
+void Stack::size()
+{
+    cout << "Size of stack is " << top+1 << endl;
+}
+
+void Stack::isFull()
+{
+    if(top+1 == capacity)
+        cout << "Stack is full" << endl;
+    else
+        cout << "Stack is not full" << endl;
+}
+
 int main()
 {
     Stack s(3);
     s.push(1);
     s.push(2);
+    s.isFull();
     s.push(3);
+    s.isFull();
     s.push(4);
+    s.isFull();
     s.pop();
     s.peek();
     s.isEmpty();
     s.pop();
     s.isEmpty();
+    s.isFull();
     s.pop();
     s.isEmpty();
     s.peek();
     s.pop();
+    s.isFull();
     s.isEmpty();
     s.peek();
     return 0;
